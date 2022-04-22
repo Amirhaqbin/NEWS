7c1858680e69e41fc974e6021a0f005f1234ca33
Amirhaqbin <awmirhb1212@gmail.com>
2022-04-22 14:19:42 +0430

    models updated

--- khabari/models.py
+++ khabari/models.py
@@ -2,13 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class NewsPage(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
-    genre = models.CharField(max_length=100)
-    ratings = models.ManyToManyField(User, blank=True, through='NewsRating')
+    title = models.CharField(max_length=128)
+    author = models.CharField(max_length=128)
+    genre = models.CharField(max_length=128)
+    ratings = models.ManyToManyField(User, blank=True, through='Comment')
     rate = models.FloatField(blank=True, null=True)
-    text = models.TextField(blank=True, null=True)
-    image = models.ImageField(upload_to='library/static/covers/', default='static/covers/default-cover.jpeg')
+    news_text = models.TextField(blank=True, null=True)
+    cover = models.ImageField(
+        upload_to='library/static', default='')
     view_count = models.IntegerField(default=0)
     published_at = models.DateTimeField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -17,15 +18,18 @@ class NewsPage(models.Model):
     def __str__(self):
         return self.title
 
-class NewsRating(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    news = models.ForeignKey(NewsPage, on_delete=models.CASCADE)
-    rating = models.SmallIntegerField(choices=[(i, i) for i in range(1, 6)], null=True, blank=True)
-
-    class Meta:
-        unique_together = ('user', 'news')
 
 class Comment(models.Model):
-    user = models.ForeignKey(User, related_name='comments', on_delete=models.CASCADE)
-    news = models.ForeignKey(NewsPage, related_name='comments', on_delete=models.CASCADE)
-    message = models.TextField()
+    user = models.ForeignKey(
+        User, related_name='comments', on_delete=models.CASCADE)
+    news = models.ForeignKey(
+        NewsPage, related_name='comments', on_delete=models.CASCADE)
+    text = models.TextField()
+    rating = models.SmallIntegerField(
+        choices=[(i, i) for i in range(1, 6)], null=True, blank=True)
+
+    class Meta:
+        unique_together = (
+            'news',
+            'user'
+        )
